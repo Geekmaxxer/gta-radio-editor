71ac5d9a8c21cfbc139afd82f214a6b0f1029114
Pew <83255316+Geekmaxxer@users.noreply.github.com>
2026-09-04 15:44:00 -0700

    Fix update dialog

--- GTARadioEditor/UpdateAvailableDialog.cs
+++ GTARadioEditor/UpdateAvailableDialog.cs
@@ -9,7 +9,6 @@ public sealed class UpdateAvailableDialog : Form
     public UpdateAvailableDialog(string currentVersion, string latestVersion, string releaseUrl)
     {
         this.releaseUrl = releaseUrl;
-
         Text = "Update Available";
         Font = SystemFonts.MessageBoxFont;
         FormBorderStyle = FormBorderStyle.FixedDialog;
@@ -17,58 +16,50 @@ public sealed class UpdateAvailableDialog : Form
         MinimizeBox = false;
         ShowInTaskbar = false;
         StartPosition = FormStartPosition.CenterParent;
-        ClientSize = new Size(380, 168);
-
+        ClientSize = new Size(380, 170);
         var iconBox = new PictureBox
         {
             Image = SystemIcons.Information.ToBitmap(),
             SizeMode = PictureBoxSizeMode.AutoSize,
             Location = new Point(20, 20)
         };
-
         var messageLabel = new Label
         {
-            Text = "A new version of GTA Radio Editor is available.",
+            Text = "A new version of GTA Gameconfig Updater is available.",
             AutoSize = true,
             Location = new Point(64, 20),
             MaximumSize = new Size(296, 0)
         };
-
         var currentVersionLabel = new Label
         {
-            Text = $"Current version:   {currentVersion}",
+            Text = $"Current version: {currentVersion}",
             AutoSize = true,
             Location = new Point(64, 56)
         };
-
         var newVersionLabel = new Label
         {
-            Text = $"New version:        {latestVersion}",
+            Text = $"New version: {latestVersion}",
             AutoSize = true,
             Font = new Font(Font, FontStyle.Bold),
             Location = new Point(64, 76)
         };
-
+        var goButton = new Button
+        {
+            Text = "Take me there",
+            Size = new Size(110, 32),
+            Location = new Point(80, 120),
+            UseVisualStyleBackColor = true
+        };
         var dismissButton = new Button
         {
             Text = "I'm good",
             Size = new Size(110, 32),
-            Location = new Point(150, 120),
+            Location = new Point(198, 120),
             DialogResult = DialogResult.Cancel
         };
-
-        var goButton = new Button
-        {
-            Text = "Take me there",
-            Size = new Size(130, 32),
-            Location = new Point(268, 120),
-            UseVisualStyleBackColor = true
-        };
         goButton.Click += GoButton_Click;
-
         AcceptButton = goButton;
         CancelButton = dismissButton;
-
         Controls.Add(iconBox);
         Controls.Add(messageLabel);
         Controls.Add(currentVersionLabel);
